619644ad37c4ff484d8f64adfeac98c49a4a215e
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-02-26 12:34:37 -0300

    Add files via upload

---
+++ dowhile.js
@@ -0,0 +1,7 @@
+var matriz = ["vetor1","vetor2","vetor3","vetor4","vetor5"]; //nomes das declaraçoes de variaveis
+var posicao = 0; //declaracao de variavel;
+
+while (posicao < matriz.length) { //enquanto a posicao ser menor q a matriz;
+    console.log(matriz[posicao]); //estrutura de repetiçao;
+    posicao++; // posicao - posicao +1;
+}
